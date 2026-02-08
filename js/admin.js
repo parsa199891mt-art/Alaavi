e3e768269b8ff0c94fd552979de813b84b2d9414
parsa199891mt-art <parsa199891mt@gmail.com>
2026-02-08 18:48:45 +0330

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,40 +1,53 @@
-const ADMIN_PASSWORD = "1234";
+const ADMIN_PASSWORD = "parsa1998";
+const albumSelect = document.getElementById("albumSelect");
 
 function login() {
-  const pass = document.getElementById("password").value;
+  if (document.getElementById("password").value === ADMIN_PASSWORD) {
+    loginBox.classList.add("hidden");
+    adminPanel.classList.remove("hidden");
+    loadAlbums();
+  } else alert("رمز اشتباه است");
+}
 
-  if (pass === ADMIN_PASSWORD) {
-    document.getElementById("loginBox").classList.add("hidden");
-    document.getElementById("adminPanel").classList.remove("hidden");
-  } else {
-    alert("رمز اشتباه است");
-  }
+function loadAlbums() {
+  const albums = JSON.parse(localStorage.getItem("albums")) || [];
+  albumSelect.innerHTML = `<option value="">انتخاب آلبوم</option>`;
+  albums.forEach(a => {
+    const opt = document.createElement("option");
+    opt.value = a.name;
+    opt.textContent = a.name;
+    albumSelect.appendChild(opt);
+  });
 }
 
 function uploadImage() {
-  const albumName = document.getElementById("albumName").value;
-  const file = document.getElementById("imageFile").files[0];
-
-  if (!albumName || !file) {
-    alert("همه فیلدها رو پر کن");
-    return;
-  }
+  const albumName = newAlbum.value || albumSelect.value;
+  const file = imageFile.files[0];
+  if (!albumName || !file) return;
 
   const reader = new FileReader();
   reader.onload = () => {
     let albums = JSON.parse(localStorage.getItem("albums")) || [];
-
     let album = albums.find(a => a.name === albumName);
-    if (!album) {
-      album = { name: albumName, images: [] };
-      albums.push(album);
-    }
-
+    if (!album) albums.push(album = { name: albumName, images: [] });
     album.images.push(reader.result);
     localStorage.setItem("albums", JSON.stringify(albums));
-
-    alert("عکس ذخیره شد ✅");
+    loadAlbums();
   };
-
   reader.readAsDataURL(file);
+}
+
+function deleteLast() {
+  let albums = JSON.parse(localStorage.getItem("albums")) || [];
+  let album = albums.find(a => a.name === albumSelect.value);
+  if (!album) return;
+  album.images.pop();
+  localStorage.setItem("albums", JSON.stringify(albums));
+}
+
+function deleteAlbum() {
+  let albums = JSON.parse(localStorage.getItem("albums")) || [];
+  albums = albums.filter(a => a.name !== albumSelect.value);
+  localStorage.setItem("albums", JSON.stringify(albums));
+  loadAlbums();
 }
